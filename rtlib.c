6554b2c14e0092b48c36b6958745bbf61fc490b2
kavoor <kev29@cornell.edu>
2019-11-10 19:37:06 -0500

    rtlib

--- rtlib.c
+++ rtlib.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
+#include <string.h> 
+#include <limits.h>
+#include <stdbool.h>
+
+int map[INT_MAX];
+bool init = 0;
+
 void logop(int i) {
-    printf("computed: %i\n", i);
+    if( !init ){
+        memset(map, 0, INT_MAX);
+        init = 1;
+    }
 }
+
+void inc(int key){
+    map[key] = map[key] + 1;
+    printf("%d, %d", key, map[key]);
+}
+

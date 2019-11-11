70819133bc71562d2692a2ce2d322eb69f9f245b
kavoor <kev29@cornell.edu>
2019-11-10 19:45:22 -0500

    rtlib

--- rtlib.c
+++ rtlib.c
@@ -6,15 +6,17 @@
 int map[INT_MAX];
 bool init = 0;
 
-void logop(int i) {
-    if( !init ){
-        memset(map, 0, INT_MAX);
-        init = 1;
-    }
-}
 
 void inc(int key){
     map[key] = map[key] + 1;
     printf("%d, %d \n", key, map[key]);
 }
 
+
+void logop(int i) {
+    if( !init ){
+        memset(map, 0, INT_MAX);
+        init = 1;
+    }
+    inc(i);
+}

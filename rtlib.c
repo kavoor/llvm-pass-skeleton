46f5d121ce6fd9d38b1cfeff446353cff72dd50b
Henry Liu <liuhenry4428@gmail.com>
2019-11-13 20:09:55 -0500

    Fixed source/dest tracking

--- rtlib.c
+++ rtlib.c
@@ -7,22 +7,25 @@
 int srcmap[INT_MAX];
 int destmap[INT_MAX];
 bool init = 0;
+bool previousSource = false;
+int previousSourceNum;
 
 void logsrc(int i) {
-    if( !init ){
-        memset(srcmap, 0, INT_MAX);
-        init = 1;
-    }
-    srcmap[i]++;
-    printf(">>SOURCE %d, %d \n", i, srcmap[i]);
+    previousSource = true;
+    previousSourceNum = i;
 }
 
 
 void logdest(int i) {
+    if(!previousSource) return;
     if( !init ){
+        memset(srcmap, 0, INT_MAX);
         memset(destmap, 0, INT_MAX);
         init = 1;
     }
     destmap[i]++;
+    srcmap[previousSourceNum]++;
+    printf(">>SOURCE %d, %d \n", previousSourceNum, srcmap[previousSourceNum]);
     printf(">>DEST %d, %d \n", i, destmap[i]);
+    previousSource = false;
 }

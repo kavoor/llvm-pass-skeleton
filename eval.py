218be1cf8d5b8cbc5773ce0b07f8cf38ae7cf7db
Henry Liu <liuhenry4428@gmail.com>
2019-11-14 02:51:11 -0500

    shhhh no more errors

--- eval.py
+++ eval.py
@@ -11,7 +11,7 @@ for filename in filenames:
     for _ in range(NUM_ITERATIONS):
         # filename = 'test-suite/Stanford/' + filename
         os.system('/usr/local/opt/llvm@9/bin/clang -S -emit-llvm -Xclang -disable-O0-optnone test-suite/Stanford/' + filename + '.c ')
-        os.system('opt -load build/skeleton/libSkeletonPass.* -skeleton -S ' + filename + '.ll -o ' + filename + '1.ll > /dev/null')
+        os.system('opt -load build/skeleton/libSkeletonPass.* -skeleton -S ' + filename + '.ll -o ' + filename + '1.ll &> /dev/null')
         os.system('llvm-link rtlib.ll ' + filename + '1.ll ' + '-o link.ll')
         os.system('llc link.ll; clang link.s')
         start = time.time()
@@ -28,7 +28,7 @@ for filename in filenames:
     for _ in range(NUM_ITERATIONS):
         # filename = 'test-suite/Stanford/' + filename
         os.system('/usr/local/opt/llvm@9/bin/clang -S -emit-llvm -Xclang -disable-O0-optnone test-suite/Stanford/' + filename + '.c ')
-        os.system('opt -load build/skeleton/libSkeletonPass.* -skeleton -S ' + filename + '.ll -o ' + filename + '1.ll > /dev/null')
+        os.system('opt -load build/skeleton/libSkeletonPass.* -skeleton -S  ' + filename + '.ll -o ' + filename + '1.ll &> /dev/null')
         os.system('opt -O3 ' + filename + '1.ll -o ' + filename + '2.ll > /dev/null')
         os.system('llvm-link rtlib.ll ' + filename + '2.ll ' + '-o link.ll')
         os.system('llc link.ll; clang link.s')
@@ -65,8 +65,8 @@ for filename in filenames:
 import plotly.graph_objects as go
 
 fig = go.Figure(data=[
-    go.Bar(name='instrumented', x=filenames, y=[results1[filename] for filename in filenames]),
-    go.Bar(name='nah', x=filenames, y=[results2[filename] for filename in filenames])
+    go.Bar(name='Profiled', x=filenames, y=[results1[filename] for filename in filenames]),
+    go.Bar(name='Profiled and -O3', x=filenames, y=[results2[filename] for filename in filenames])
 ])
 # Change the bar mode
 fig.update_layout(barmode='group')

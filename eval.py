4867dd1326a41d0fd4ff3ae0704cd28f1c26ab52
kavoor <kev29@cornell.edu>
2019-11-14 02:04:17 -0500

    [eval] graph

--- eval.py
+++ eval.py
@@ -12,9 +12,27 @@ for j in range(len(cmds)):
             os.system(cmd)
             end = time.time()
             sum = sum + (end - start)
-            
+
         # average
         avg = sum / (5)
         result.append(avg)
     results.append(result)
 
+# Create traces
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=x1, y=y1,
+                    mode='lines+markers',
+                    name='vector',text="vector",
+                    error_y=dict(
+            type='data', # value of error bar given in data coordinates
+            array=std1,
+            visible=True)))
+fig.add_trace(go.Scatter(x=x0, y=y0,
+                    mode='lines+markers',
+                    name='serial',text="serial",
+                     error_y=dict(
+            type='data', # value of error bar given in data coordinates
+            array=std0,
+            visible=True)))
+
+fig.show()

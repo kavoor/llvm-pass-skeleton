1b469fc733d62c5dffbf7054ad649efa98681d27
Henry Liu <liuhenry4428@gmail.com>
2019-11-14 22:54:42 -0500

    aaaaaaaa

--- rtlib.c
+++ rtlib.c
@@ -8,7 +8,6 @@
 // int destmap[INT_MAX];
 int WTF[__INT16_MAX__][__INT16_MAX__] = {};
 
-bool init = 0;
 bool previousSource = false;
 int previousSourceNum;
 
@@ -20,17 +19,7 @@ void logsrc(int i) {
 
 void logdest(int i) {
     if(!previousSource) return;
-    // if( !init ){
-    //     memset(srcmap, 0, INT_MAX);
-    //     memset(destmap, 0, INT_MAX);
-    //     init = 1;
-    // }
-    // destmap[i]++;
-    // srcmap[previousSourceNum]++;
     WTF[previousSourceNum][i]++;
-    // printf(">>SOURCE %d, DEST %d, COUNT %d \n", previousSourceNum, i, WTF[previousSourceNum][i]);
-    // printf(">>SOURCE %d, %d \n", previousSourceNum, srcmap[previousSourceNum]);
-    // printf(">>DEST %d, %d \n", i, destmap[i]);
     previousSource = false;
 }
 
